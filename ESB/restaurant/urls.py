114c2e334fe71b78a28e41e8dbe410c4d5420b68
artibuk <artibuk1052@icloud.com>
2023-04-19 20:48:15 +0700

    paginator

--- ESB/restaurant/urls.py
+++ ESB/restaurant/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
    path('<int:category_id>',restaurant,name='category'),
    path('<int:access_id>',restaurant,name='perm'),
    path('report/<int:restaurant_id>', report, name='report'),
+   path('page/<int:page>', restaurant, name='restaurant_pagination'),
 ]
--- ESB/restaurant/views.py
+++ ESB/restaurant/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from restaurant.models import Restaurant,RestaurantCategory
 from user.models import User,RightUser
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 
 def index(request):
@@ -26,8 +27,12 @@ def restaurant(request, category_id=None):
     # Если категория не выбрана, выводим все доступные рестораны для пользователя
     else:
         restaurants = Restaurant.objects.filter(perm_grup_fo=user.access_rights)
+        
+    paginator = Paginator(restaurants, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context.update({
-        'restaurant': restaurants,
+        'restaurant': page_obj,
     })
     return render(request, 'restaurant/restaurant.html', context)
 
