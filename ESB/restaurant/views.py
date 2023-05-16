7cd9f94b4bf6e0f1217b6fd9dbfe62a769d8668f
artibuk <artibuk1052@icloud.com>
2023-05-16 22:46:43 +0700

    last_fix_of_report

--- ESB/restaurant/views.py
+++ ESB/restaurant/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
 from restaurant.models import Restaurant, RestaurantCategory, Report, ReportType
-from django.db.models import Q
 from django.core.paginator import Paginator
 import datetime
 import plotly.graph_objs as go
 from django.db.models import Sum
-from django.db.models import F
-from decimal import Decimal
+from dateutil.relativedelta import relativedelta
 
 
 def index(request):
@@ -172,46 +170,152 @@ def consolidated_report(request, report_type_id):
 
     return render(request, 'restaurant/consolidated_report.html', context)
 
-
 def week_report(request, report_type_id):
     user = request.user
     report_type = ReportType.objects.get(pk=report_type_id)
-    accessible_restaurants = Restaurant.objects.filter(
-        perm_grup_fo=user.access_rights)
+    accessible_restaurants = Restaurant.objects.filter(perm_grup_fo=user.access_rights)
 
     if report_type_id == 3:
         report_name = report_type.name_report
-        if 'week' in request.GET and 'year' in request.GET:
-            # если переданы номер недели и год, то фильтруем данные за эту неделю
+        if 'week' in request.GET and 'year' in request.GET and 'compare_week' in request.GET and 'compare_year' in request.GET:
             year = int(request.GET['year'])
             week_number = int(request.GET['week'])
-            start_date = datetime.datetime.strptime(
-                f'{year}-W{week_number}-1', "%G-W%V-%u").date()
+            compare_year = int(request.GET['compare_year'])
+            compare_week_number = int(request.GET['compare_week'])
+            previous_year = year - 1
+            #все переменные даты
+            start_date = datetime.datetime.strptime(f'{year}-W{week_number}-1', "%G-W%V-%u").date()
             end_date = start_date + datetime.timedelta(days=6)
-
+            previous_start_date = start_date.replace(year=previous_year)
+            previous_end_date = end_date.replace(year=previous_year)
+            compare_start_date = datetime.datetime.strptime(f'{compare_year}-W{compare_week_number}-1', "%G-W%V-%u").date()
+            compare_end_date = compare_start_date + datetime.timedelta(days=6)
+            start_date_from = datetime.date(start_date.year, start_date.month, 1)
+            end_date_from = start_date_from + relativedelta(months=1, days=-1)
+            end_date_from = min(end_date_from, end_date)
+            previous_start_date_from = datetime.date(previous_start_date.year, previous_start_date.month, 1)
+            previous_end_date_from = previous_start_date_from + relativedelta(months=1, days=-1)
+            previous_end_date_from = min(previous_end_date_from, previous_end_date)
+            days_from_start = (end_date_from - start_date_from).days+1
+            print(days_from_start)
+            #вытаскивания данных по датам
             report_data = Report.objects.filter(
-                department__in=accessible_restaurants,  # filter by accessible restaurants
-                data__gte=start_date,
-                data__lte=end_date,
+                department__in=accessible_restaurants,
+                data__range=(start_date, end_date),
                 department__isnull=False
-            ).select_related('department').values('department__name', 'department__city').annotate(
-                revenue=Sum('revenue'),
-                cost_price=Sum('cost_price'),
-                number_of_checks=Sum('number_of_checks')
-            ).order_by('department')
-        else:
-            report_data = None
+            ).values('department__category__name', 'department__name', 'department__city').annotate(
+                revenue_selected_week=Sum('revenue')
+            ).order_by('department__category', 'department')
 
-        context = {
-            'title': 'NRG',
-            'report_name': report_name,
-            'report_data': report_data,
-            'current_month': None,
-            'current_year': None,
-            'report_type_id': report_type_id,
-        }
+            compare_report_data = Report.objects.filter(
+                department__in=accessible_restaurants,
+                data__range=(compare_start_date, compare_end_date),
+                department__isnull=False
+            ).values('department__category__name', 'department__name', 'department__city').annotate(
+                compare_revenue_selected_week=Sum('revenue')
+            ).order_by('department__category', 'department')
+            previous_year_data = Report.objects.filter(
+                department__in=accessible_restaurants,
+                data__range=(previous_start_date, previous_end_date),
+                department__isnull=False
+            ).values('department__category__name', 'department__name', 'department__city').annotate(
+                revenue_for_last_year=Sum('revenue')
+            ).order_by('department__category', 'department')
+            departments_with_revenue = {(item['department__name'], item['department__city']) for item in previous_year_data}
+            # Преобразование Decimal во float
+            previous_year_data = [
+                {
+                    'department__category__name': item['department__category__name'],
+                    'department__name': item['department__name'],
+                    'department__city': item['department__city'],
+                    'revenue_for_last_year': float(item['revenue_for_last_year'] or 0),
+                }
+                for item in previous_year_data
+            ]
+            # Добавление нулевых значений для ресторанов без данных о доходах за прошлый год
+            departments_without_revenue = {(item['department__name'], item['department__city']) for item in report_data} - departments_with_revenue
+            for department in departments_without_revenue:
+                previous_year_data.append({
+                    'department__category__name': '',
+                    'department__name': department[0],
+                    'department__city': department[1],
+                    'revenue_for_last_year': 0.0,
+                })
+            report_data_from = Report.objects.filter(
+                department__in=accessible_restaurants,
+                data__range=(start_date_from,end_date_from),
+                department__isnull=False
+            ).values('department__category__name', 'department__name', 'department__city').annotate(
+                revenue_from=Sum('revenue')
+            ).order_by('department__category', 'department')
+            previous_report_data_from = Report.objects.filter(
+                department__in=accessible_restaurants,
+                data__range=(previous_start_date_from, previous_end_date_from),
+                department__isnull=False
+            ).values('department__category__name', 'department__name', 'department__city').annotate(
+                previous_revenue_from=Sum('revenue')
+            ).order_by('department__category', 'department')
 
-        return render(request, 'restaurant/week_results.html', context)
+            departments_with_revenue_from = {(item['department__name'], item['department__city']) for item in previous_report_data_from}
+
+            previous_report_data_from = [
+                {
+                    'department__category__name': item['department__category__name'],
+                    'department__name': item['department__name'],
+                    'department__city': item['department__city'],
+                    'previous_revenue_from': float(item['previous_revenue_from'] or 0),
+                }
+                for item in previous_report_data_from
+            ]
+
+            departments_without_revenue_from = {(item['department__name'], item['department__city']) for item in report_data} - departments_with_revenue_from
+            for department in departments_without_revenue_from:
+                previous_report_data_from.append({
+                    'department__category__name': '',
+                    'department__name': department[0],
+                    'department__city': department[1],
+                    'previous_revenue_from': 0.0,
+                })
+            context = {
+                'report_name': report_name,
+                'report_data': report_data,
+                'compare_report_data': compare_report_data,
+                'previous_year_data': previous_year_data,
+                'current_year': year,
+                'current_week': week_number,
+                'compare_year': compare_year,
+                'compare_week': compare_week_number,
+                'report_type_id': report_type_id,
+                'report_data_from':report_data_from,
+                'previous_report_data_from':previous_report_data_from,
+                'first_day_number':start_date_from,
+                'end_day_number':days_from_start,
+            }
+
+            return render(request, 'restaurant/week_results.html', context)
+
+    # Handle case when parameters are missing or invalid
+    report_data = None
+    compare_report_data = None
+    previous_year_data = None
+    context = {
+        'title': 'NRG',
+        'report_name': report_name,
+        'report_data': report_data,
+        'compare_report_data': compare_report_data,
+        'previous_year_data': previous_year_data,
+        'current_year': None,
+        'current_week': None,
+        'compare_year': None,
+        'compare_week': None,
+        'report_type_id': report_type_id,
+        'report_data_from':None,
+        'previous_report_data_from':None,
+        'first_day_number':None,
+        'end_day_number':None,
+    }
+
+    return render(request, 'restaurant/week_results.html', context)
 
 
 def mounth_report(request, report_type_id):
